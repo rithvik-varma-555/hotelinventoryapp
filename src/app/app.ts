df3c5bdbe5556babb93b15d365966507814d77cd
Rithvik Varma <rithvikvarmaa@gmail.com>
2025-10-03 15:33:26 -0400

    test commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Rooms } from './rooms/rooms';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'hinv-root',
+  imports: [RouterOutlet, Rooms],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('hotelinventoryapp');
