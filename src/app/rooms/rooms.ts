dd0e9650522134e00afe8b431d0668c085e6034c
Rithvik Varma <rithvikvarmaa@gmail.com>
2025-10-06 14:51:22 -0400

    basic binding syntax

--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'hinv-rooms',
   imports: [],
   templateUrl: './rooms.html',
-  styleUrl: './rooms.scss'
+  styleUrl: './rooms.scss',
 })
 export class Rooms {
+  hotelName = 'Hilton Hotel';
 
+  numberOfRooms = 10;
+
+  hideRooms = false;
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
 }
