81b25a9a6d4e14d78ad7e68df89c074d8baf8058
Rithvik Varma <rithvikvarmaa@gmail.com>
2025-10-07 14:47:16 -0400

    added rooms list

---
+++ src/app/rooms/rooms.interface.ts
@@ -0,0 +1,15 @@
+export interface Room {
+  totalRooms: number;
+  availableRooms: number;
+  bookedRooms: number;
+}
+
+export interface RoomsList {
+  roomNumber: Number;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photos: string;
+  checkinTime: Date;
+  checkoutTime: Date;
+}
