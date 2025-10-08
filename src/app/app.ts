7325c30dfea10f3b1ed5f19305a3f686fd7f864f
Rithvik Varma <rithvikvarmaa@gmail.com>
2025-10-08 19:49:02 -0400

    added switch case for login (basic)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Rooms } from './rooms/rooms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'hinv-root',
-  imports: [RouterOutlet, Rooms],
+  imports: [RouterOutlet, Rooms, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('hotelinventoryapp');
+
+  role = 'admin';
 }
