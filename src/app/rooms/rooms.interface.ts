ff9c1d867370690c53dfa0bdd9a85be9dfd10ee7
Rithvik Varma <rithvikvarmaa@gmail.com>
2025-10-15 10:58:15 -0400

    Used pipes concept in angular

--- src/app/rooms/rooms.interface.ts
+++ src/app/rooms/rooms.interface.ts
@@ -12,4 +12,5 @@ export interface RoomsList {
   photos: string;
   checkinTime: Date;
   checkoutTime: Date;
+  rating: number;
 }
--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -29,6 +29,7 @@ export class Rooms {
         'https://images.unsplash.com/photo-1560448070-c6d4f5f1c8b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVsdXhlJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
       checkinTime: new Date('2021-11-11'),
       checkoutTime: new Date('2021-11-12'),
+      rating: 4.5,
     },
     {
       roomNumber: 2,
@@ -39,6 +40,7 @@ export class Rooms {
         'https://images.unsplash.com/photo-1501117716987-c8e2bfbf0f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJlbWl1bSUyMHJvb218ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       checkinTime: new Date('2021-11-11'),
       checkoutTime: new Date('2021-11-12'),
+      rating: 3.4,
     },
     {
       roomNumber: 3,
@@ -49,6 +51,7 @@ export class Rooms {
         'https://images.unsplash.com/photo-1501183638714-8f3c5a7b8f4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bHV4dXJ5JTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
       checkinTime: new Date('2021-11-11'),
       checkoutTime: new Date('2021-11-12'),
+      rating: 2.6,
     },
   ];
 
